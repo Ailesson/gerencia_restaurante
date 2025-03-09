4678c5c1babe9f15154e61ad0aca74cb6b135650
Ailesson <128842954+Ailesson@users.noreply.github.com>
2025-03-09 18:52:33 -0400

    Update tableservice.tsx

--- frontend/src/app/services/tableservice.tsx
+++ frontend/src/app/services/tableservice.tsx
@@ -19,7 +19,6 @@ export const getTables = async (): Promise<pedido4mesa_response[]> => {
     return response.data;
 };
 
-// Criar pedido com o tipo correto
 export const createTable = async (): Promise<mesa_response> => {
     const response = await api.post("/tables");
     return response.data;
@@ -28,9 +27,8 @@ export const createTable = async (): Promise<mesa_response> => {
 export const closeTable = async (mesaId: Number): Promise<{ message: string }> => {
     console.log('request chegou');
     
-    // Fazendo a requisição POST com o mesaId como query parameter
     const response = await api.post(`/tables/${mesaId}/close`, null, {
-        params: { mesa_id: mesaId },  // Query parameter
+        params: { mesa_id: mesaId },
     });
     
     console.log(response.data);
